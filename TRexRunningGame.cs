1a7662dcc9a47f653e55fbb95d53132781ffaa75
strrojas <79352024+strrojas@users.noreply.github.com>
2021-02-20 01:31:22 -0500

    Create TRexRunningGame.cs

---
+++ TRexRunningGame.cs
@@ -0,0 +1,91 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TrexRunner.Entities;
+using TrexRunner.Graphics;
+
+namespace TrexRunner
+{
+    public class TRexRunningGame : Game
+    {
+        public const int WINDOW_WIDTH = 600;
+        public const int WINDOW_HEIGHT = 150;
+
+        private const string ASSET_NAME_SPRITESHEET = "TrexSpritesheet";
+        private const string ASSET_NAME_SFX_HIT = "hit";
+        private const string ASSET_NAME_SFX_SCORE_REACHED = "score-reached";
+        private const string ASSET_NAME_SFX_BUTTON_PRESS = "button-press";
+
+        public const int TREX_START_POS_Y = WINDOW_HEIGHT - 16;
+        public const int TREX_START_POS_X = 1;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private SoundEffect _sfxHit;
+        private SoundEffect _sfxButtonPress;
+        private SoundEffect _sfxScoreReached;
+
+        private Texture2D _spriteSheetTexture;
+
+        private Trex _trex;
+
+        public TRexRunningGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            _graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            _graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _sfxButtonPress = Content.Load<SoundEffect>(ASSET_NAME_SFX_BUTTON_PRESS);
+            _sfxHit = Content.Load<SoundEffect>(ASSET_NAME_SFX_HIT);
+            _sfxScoreReached = Content.Load<SoundEffect>(ASSET_NAME_SFX_SCORE_REACHED);
+
+            _spriteSheetTexture = Content.Load<Texture2D>(ASSET_NAME_SPRITESHEET);
+
+            _trex = new Trex(_spriteSheetTexture, new  Vector2(TREX_START_POS_X,TREX_START_POS_Y - Trex.TREX_DEFAULT_SPRITE_HEIGHT));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+
+            _trex.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _trex.Draw(_spriteBatch, gameTime);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
