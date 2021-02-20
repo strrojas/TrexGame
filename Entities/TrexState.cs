02b409fbfaa82d76926efe875d06a00c6d3323a6
strrojas <79352024+strrojas@users.noreply.github.com>
2021-02-20 01:26:54 -0500

    Create TrexState.cs

---
+++ Entities/TrexState.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrexRunner.Entities
+{
+   public enum TrexState
+    {
+        Idle,
+        Running,
+        Jumping,
+        Ducking,
+        Falling,
+    }
+}
